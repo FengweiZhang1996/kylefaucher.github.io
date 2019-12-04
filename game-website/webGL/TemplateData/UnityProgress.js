ac416d3300db17f20497deef5e277b4d1883168e
Kyle Faucher <kyle.faucher@colorado.edu>
2019-12-03 19:14:02 -0700

    style changes and unity progress check

--- game-website/webGL/TemplateData/UnityProgress.js
+++ game-website/webGL/TemplateData/UnityProgress.js
@@ -19,6 +19,9 @@ function UnityProgress(unityInstance, progress) {
   }
   unityInstance.progress.full.style.width = (100 * progress) + "%";
   unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
-  if (progress == 1)
+  if (progress == 1){
     unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
+    console.log("*****GAME LOADED******");
+    console.log("!!!!!!!" + firebase.auth().currentUser.displayName);
+  }
 }
